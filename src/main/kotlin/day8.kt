b942ef621ee5ad0a31033ad87c970a4d9f9ce117
Juraj Noge <juraj.noge@productboard.com>
2022-12-08 09:13:47 +0100

    Day 8 - clean solution

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -6,42 +6,32 @@ class Trees : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
         val trees = readInputLines("8.txt").flatMapIndexed { y, row ->
             row.mapIndexed { x, height -> Coordinate(x, y) to Character.getNumericValue(height) }
-        }.toMap()
+        }.toMap().withDefault { -1 }
 
-        data class TreePosition(val viewDistance: Int, val hidden: Boolean) {
+        data class TreePosition(val viewDistance: Int, val blocked: Boolean) {
             infix fun combineWith(other: TreePosition) =
-                TreePosition(viewDistance * other.viewDistance, hidden && other.hidden)
+                TreePosition(viewDistance * other.viewDistance, blocked && other.blocked)
         }
 
-        fun calculatePosition(coordinate: Coordinate, height: Int, moveFn: (Coordinate) -> Coordinate): TreePosition {
-            var viewDistance = 0
-            var hidden = false
-            var current = coordinate
-            while (true) {
-                val next = moveFn(current)
-                if (next !in trees) {
-                    break
-                }
-                if (trees.getValue(next) >= height) {
-                    viewDistance += 1
-                    hidden = true
-                    break
-                }
-                viewDistance += 1
-                current = next
-            }
-
-            return TreePosition(viewDistance, hidden)
-        }
+        fun calculatePosition(
+            initialCoordinate: Coordinate,
+            nextCoordinate: (Coordinate) -> Coordinate,
+            height: Int
+        ): TreePosition =
+            generateSequence(initialCoordinate to TreePosition(0, false)) { (current, position) ->
+                val next = nextCoordinate(current)
+                next to TreePosition(position.viewDistance + 1, trees.getValue(next) >= height)
+            }.takeWhile { (current, _) -> current in trees }
+                .run { firstOrNull { (_, position) -> position.blocked } ?: last() }.second
 
         return trees.map { (coordinate, height) ->
-            val left = calculatePosition(coordinate, height, Coordinate::left)
-            val right = calculatePosition(coordinate, height, Coordinate::right)
-            val down = calculatePosition(coordinate, height, Coordinate::up)
-            val up = calculatePosition(coordinate, height, Coordinate::down)
+            val left = calculatePosition(coordinate, Coordinate::left, height)
+            val right = calculatePosition(coordinate, Coordinate::right, height)
+            val down = calculatePosition(coordinate, Coordinate::up, height)
+            val up = calculatePosition(coordinate, Coordinate::down, height)
 
             listOf(left, right, down, up).reduce { pos1, pos2 -> pos1 combineWith pos2 }
-        }.run { if (part2) maxOf { it.viewDistance } else count { !it.hidden } }
+        }.run { if (part2) maxOf { it.viewDistance } else count { !it.blocked } }
     }
 }
 

0b0ee4306525ea0e828d3bb8c21f5f65471be310
Juraj Noge <juraj.noge@productboard.com>
2022-12-13 10:34:25 +0100

    Days 11 - clean

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,70 +1,48 @@
+import utils.readInputBlock
 import kotlin.math.floor
 
 /** [https://adventofcode.com/2021/day/11] */
 class Monkeys : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        data class Monkey(val items: MutableList<Long>, val operation: (Long) -> Long, val test: (Long) -> Int) {
+        data class Monkey(
+            val items: MutableList<Long>,
+            val inspect: (Long) -> Long,
+            val testDivisor: Int,
+            val testOk: Int,
+            val testFail: Int
+        ) {
             var inspectCount = 0L
         }
 
-        val totalDivisor = 19 * 2 * 3 * 17 * 13 * 7 * 5 * 11
-        val monkeys = listOf(
-            Monkey(
-                items = mutableListOf(74, 73, 57, 77, 74),
-                operation = { it * 11 },
-                test = { if (it % 19 == 0L) 6 else 7 }
-            ),
-            Monkey(
-                items = mutableListOf(99, 77, 79),
-                operation = { it + 8 },
-                test = { if (it % 2 == 0L) 6 else 0 }
-            ),
-            Monkey(
-                items = mutableListOf(64, 67, 50, 96, 89, 82, 82),
-                operation = { it + 1 },
-                test = { if (it % 3 == 0L) 5 else 3 }
-            ),
-            Monkey(
-                items = mutableListOf(88),
-                operation = { it * 7 },
-                test = { if (it % 17 == 0L) 5 else 4 }
-            ),
-            Monkey(
-                items = mutableListOf(80, 66, 98, 83, 70, 63, 57, 66),
-                operation = { it + 4 },
-                test = { if (it % 13 == 0L) 0 else 1 }
-            ),
-            Monkey(
-                items = mutableListOf(81, 93, 90, 61, 62, 64),
-                operation = { it + 7 },
-                test = { if (it % 7 == 0L) 1 else 4 }
-            ),
-            Monkey(
-                items = mutableListOf(69, 97, 88, 93),
-                operation = { it * it },
-                test = { if (it % 5 == 0L) 7 else 2 }
-            ),
-            Monkey(
-                items = mutableListOf(59, 80),
-                operation = { it + 6 },
-                test = { if (it % 11 == 0L) 2 else 3 }
-            )
-        )
+        val monkeys =
+            readInputBlock("11.txt").split("\n\n").map { it.lines().filter(String::isNotBlank).drop(1) }.map { block ->
+                val items = block[0].substringAfter(": ").split(", ").map(String::toLong).toMutableList()
+                val inspect: ((Long) -> Long) = block[1].substringAfter(" = old ").split(" ").let { (sign, operand) ->
+                    if (sign == "*") {
+                        { value -> value * if (operand == "old") value else operand.toLong() }
+                    } else {
+                        { value -> value + if (operand == "old") value else operand.toLong() }
+                    }
+                }
+                val testDivisor = block[2].substringAfter("divisible by ").toInt()
+                val testOk = block[3].substringAfter("to monkey ").toInt()
+                val testFail = block[4].substringAfter("to monkey ").toInt()
+                Monkey(items, inspect, testDivisor, testOk, testFail)
+            }
 
+        val totalDivisor = monkeys.map(Monkey::testDivisor).reduce(Int::times)
         fun Monkey.turn() {
             while (items.isNotEmpty()) {
                 inspectCount += 1
                 val worryLevel =
-                    operation(items.removeFirst()).let { if (part2) it % totalDivisor else floor(it / 3.0).toLong() }
-                monkeys[test(worryLevel)].items.add(worryLevel)
+                    inspect(items.removeFirst()).let { if (part2) it % totalDivisor else floor(it / 3.0).toLong() }
+                val newMonkey = if (worryLevel % testDivisor == 0L) testOk else testFail
+                monkeys[newMonkey].items.add(worryLevel)
             }
         }
 
-        repeat(if (part2) 10_000 else 20) {
-            monkeys.forEach { it.turn() }
-        }
-
-        return monkeys.map { it.inspectCount }.sortedDescending().take(2).let { it.first() * it.last() }
+        repeat(if (part2) 10_000 else 20) { monkeys.forEach(Monkey::turn) }
+        return monkeys.map { it.inspectCount }.sortedDescending().take(2).run { first() * last() }
     }
 }
 
